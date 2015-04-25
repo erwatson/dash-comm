2c36a510056f3eb01cfccf2c23a9f9c3d3e89145
Oleg <o.yanchinskiy@gmail.com>
2015-04-25 15:16:45 -0700

    adding letters to the word

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -6,25 +6,36 @@ $(document).ready(function(){
 
   // this function will take the letterContainers and populate the button's innner html
   var updateUI = function(input){
-	  var letterContainers = document.getElementsByClassName('letterContainer');
-	  var containerLetters = letters.producesubSets(input);
-    for(var i = 0; i < letterContainers.length; i++){
-    	if(i === 0){
-    		var htmlLetters = containerLetters.firstThird;
-    		console.log(htmlLetters);
-	    	letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
-	    	letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
-    	} else if(i === 1){
-    		var htmlLetters = containerLetters.middleThird;
-    		letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
-    		letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
-    	} else {
-    		var htmlLetters = containerLetters.lastThird;
-    		letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
-    		letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
-    	}
-    }
+  	if(input.length === 1){
+  		appendAndReset(input);
+  	} else {		
+		  var letterContainers = document.getElementsByClassName('letterContainer');
+		  console.log('input', input)
+		  var containerLetters = letters.producesubSets(input);
+		  // console.log('containerLetters', containerLetters);
+	    for(var i = 0; i < letterContainers.length; i++){
+	    	if(i === 0){
+	    		var htmlLetters = containerLetters.firstThird;
+		    	letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
+		    	letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
+	    	} else if(i === 1){
+	    		var htmlLetters = containerLetters.middleThird;
+	    		letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
+	    		letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
+	    	} else {
+	    		var htmlLetters = containerLetters.lastThird;
+	    		letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
+	    		letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
+	    	}
+	    }
+  	}
   };
+
+  var appendAndReset = function(input){
+  	$('.sentence').append(input);
+  	var newletters = letters.initialLetters.split('');
+		updateUI(newletters);
+  }
 
   updateUI(letters.initialLetters.split(''));
 });
