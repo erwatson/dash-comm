51d28b4522ca4b6ad3bfffc4f3d42c000d4581dc
Oleg <o.yanchinskiy@gmail.com>
2015-04-25 14:46:34 -0700

    updated logic for the ui

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
 
   // find all of buttons (there should be 3), and call the produceSubsets function to populate the buttons 
-  var letterContainers = document.getElementsByClassName('letterContainer');
 
   var letters = new Letters("abcdefghijklmnopqrstuvwxyz");
-  console.log('letters', letterContainers);
 
   // this function will take the letterContainers and populate the button's innner html
   var updateUI = function(input){
-  	console.log('length of letterContainers', letterContainers.length)
+	  var letterContainers = document.getElementsByClassName('letterContainer');
 	  var containerLetters = letters.producesubSets(input);
     for(var i = 0; i < letterContainers.length; i++){
     	if(i === 0){
-	    	letterContainers[i].innerHTML = containerLetters.firstThird.join(' - ').toUpperCase();
+    		var htmlLetters = containerLetters.firstThird;
+    		console.log(htmlLetters);
+	    	letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
+	    	letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
     	} else if(i === 1){
-    		letterContainers[i].innerHTML = containerLetters.middleThird.join(' - ').toUpperCase();
+    		var htmlLetters = containerLetters.middleThird;
+    		letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
+    		letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
     	} else {
-    		letterContainers[i].innerHTML = containerLetters.lastThird.join(' - ').toUpperCase();
+    		var htmlLetters = containerLetters.lastThird;
+    		letterContainers[i].innerHTML = htmlLetters.join(' - ').toUpperCase();
+    		letterContainers[i].onclick = function(){ updateUI(this.innerHTML.split(' - '))};
     	}
     }
   };
